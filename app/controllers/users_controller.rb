0b55f0549db6e1a81e78dd7bc2e614d76287a5ef
Ramsey Njire <ramseynjire@gmail.com>
2020-11-22 13:59:55 +0300

    feature: add edit and update methods to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
 
@@ -10,6 +14,16 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render 'edit'
     end
   end
 
